211c6aa42ee1b123ff125e430de83b1b36e75cac
Artem Nechai <lenikol95@mail.ru>
2021-10-13 22:56:18 +0300

    update script

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -6,7 +6,20 @@ from page_loader.download import download
 
 
 def main():
-    pass
+    """Execute the page loader."""
+    parser = argparse.ArgumentParser(description='Download web page')
+    parser.add_argument(
+        '-o',
+        '--output',
+        type=str,
+        default='"/app"',
+        help='select folder where to download the page',
+    )
+    parser.add_argument('url', type=str)
+    args = parser.parse_args()
+    print(
+        download(args.output, args.url),
+    )
 
 
 if __name__ == '__main__':
