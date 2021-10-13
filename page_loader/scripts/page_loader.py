e16a68c78783c9cc3abdb0a694855886a9060089
Artem Nechai <lenikol95@mail.ru>
2021-10-13 22:18:51 +0300

    create page loader script

---
+++ page_loader/scripts/page_loader.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""This program downloads a page from the network."""
+import argparse
+
+from page_loader.download import download
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
