d677dd16a1f29ceb3316681a5528bae1f2bc28ad
Artem Nechai <lenikol95@mail.ru>
2021-10-17 00:36:54 +0300

    add docstring for test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,11 @@
 """This is a main testing module of the project."""
 
 import os
+
 from page_loader.download import download
 from page_loader.naming import get_name
+
+TEST_URL = 'https://ru.hexlet.io/courses'
 
 
 def test_get_name():
@@ -13,8 +16,14 @@ def test_get_name():
 
 
 def test_download(requests_mock, tmp_path):
-    requests_mock.get('https://ru.hexlet.io/courses', text='data')
-    download('https://ru.hexlet.io/courses', tmp_path)
-    with open(os.path.join(tmp_path, get_name('https://ru.hexlet.io/courses'))) as file:
-        content = file.read()
-        assert content == 'data'
+    """Test for download function: check file existing and its content.
+
+    Args:
+        requests_mock: mock for HTTP request.
+        tmp_path: temporary path for testing.
+    """
+    requests_mock.get(TEST_URL, text='data')
+    download(TEST_URL, tmp_path)
+    with open(os.path.join(tmp_path, get_name(TEST_URL))) as test_file:
+        test_content = test_file.read()
+        assert test_content == 'data'
