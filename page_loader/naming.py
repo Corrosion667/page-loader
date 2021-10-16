9ca1304cd0aa49ccad42d1274fa63402315b3ff0
Artem Nechai <lenikol95@mail.ru>
2021-10-16 20:48:38 +0300

    update naming module

--- page_loader/naming.py
+++ page_loader/naming.py
@@ -8,7 +8,7 @@ def get_name(url):
     """Get name for downloads depending on the URL of page.
 
     Args:
-        url: ULR of the web page.
+        url: URL of the web page.
 
     Returns:
         Generated name for files.
