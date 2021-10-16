6399858ef6ae05bc10b79205f3986cd90b57aaf0
Artem Nechai <lenikol95@mail.ru>
2021-10-16 23:48:33 +0300

    update dowload module

--- page_loader/download.py
+++ page_loader/download.py
@@ -1,6 +1,12 @@
 """Module to download web pages via their URLs."""
 
 
-def download():
-    """Docstring."""
+def download(url, directory):
+    """Docstring.
+
+    Args:
+        url: url.
+        directory: directory.
+    """
+
     pass  # noqa: WPS420
