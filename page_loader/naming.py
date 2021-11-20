39a4497fef2cd95d7efb11f9c1f6447dec40f76a
Artem Nechai <lenikol95@mail.ru>
2021-11-20 15:01:39 +0300

    update basic naming function

--- page_loader/naming.py
+++ page_loader/naming.py
@@ -1,20 +1,21 @@
 """Module for getting names depending on the URL of the page."""
 
+import pathlib
 import re
 from urllib.parse import urlparse
 
 
 def get_name(url):
-    """Get name for downloads depending on the URL of page.
+    """Get basic name for downloads depending on the URL of page.
 
     Args:
         url: URL of the web page.
 
     Returns:
-        Generated name for files.
+        Generated name.
     """
     prefix = urlparse(url).scheme
-    body = re.sub(r'^{0}...'.format(prefix), '', url)
+    body = re.sub(r'^{0}://'.format(prefix), '', url)
     return re.sub(r'[^a-zA-Z0-9]', '-', body)
 
 
@@ -39,4 +40,19 @@ def folder_name(url):
     Returns:
         Generated name for downloads directory.
     """
+    return '{0}_files'.format(get_name(url))
+
+
+def locals_name(url, link):
+    """Get name for local file depending on URL of page and link of the file.
+
+    Args:
+        url: URL of the web page.
+        link: src or href for the resource.
+
+    Returns:
+        Generated name for image file.
+    """
+    prefix = re.sub(r'[^a-zA-Z0-9]', '-', urlparse(url).netloc)
+    file_name = '{}{}'
     return '{0}_files'.format(get_name(url))
