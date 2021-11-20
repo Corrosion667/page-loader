c92966fe2b547b6b8d65053ce8e51e064111cd68
Artem Nechai <lenikol95@mail.ru>
2021-11-20 15:06:32 +0300

    create function for getting local resources names

--- page_loader/locals.py
+++ page_loader/locals.py
@@ -1,4 +1,4 @@
-"""Module for working with web images."""
+"""Module for working with local resources."""
 
 import os
 from urllib.parse import urlparse
@@ -7,37 +7,49 @@ import requests
 from bs4 import BeautifulSoup
 from page_loader.naming import folder_name
 
+LOCALS = ['img', 'link', 'script']
+
 
 def is_local(src, url):
-    """Criteria wether to download selected iamge or not.
+    """Criteria wether to download selected object or not.
 
     Args:
-        src: link to the image.
-        url: url of the web page where image is used.
+        src: link to the resource.
+        url: url of the web page where resource is used.
 
     Returns:
-        Boolean of affiliation of the image's src to local ones.
+        Boolean of affiliation of the objects's src to local ones.
     """
     domain = urlparse(url).netloc
     return urlparse(src).netloc in {domain, ''}
 
 
-def get_images_links(url):
-    """Get list of local images to be downloaded.
+def get_local_links(url):
+    """Get list of tuples: local resources and file names for them to be downloaded.
 
     Args:
         url: url of the web page to be parsed.
 
     Returns:
-        List of locally stored images.
+        List of locally stored resources and names for dowload files.
     """
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    img_tags = soup.find_all('img')
-    return [img['src'] for img in img_tags if is_local(img['src'], url)]
+    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+    tags = []
+    for each in LOCALS:
+        tags.extend(soup.find_all(each))
+    urls = []
+    for link in tags:
+        try:
+            if is_local(link['src'], url):
+                urls.append(link['src'])
+        except KeyError:
+            if is_local(link['href'], url):
+                urls.append(link['href'])
+                continue
+    return urls
 
 
-def download_images(url, directory):
+def download_locals(url, directory):
     os.mkdir(os.path.join(directory, folder_name(url)))
     links = get_images_links(url)
     for link in links:
