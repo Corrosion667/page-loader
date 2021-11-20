8f5e84fabfbb05cc3505273788dca00f3244bb41
Artem Nechai <lenikol95@mail.ru>
2021-11-20 21:29:09 +0300

    add donload locals function

--- page_loader/download.py
+++ page_loader/download.py
@@ -1,6 +1,7 @@
 """Module to download web pages via their URLs."""
 
 import os
+from urllib.parse import urlparse
 
 import requests
 from page_loader.naming import html_name
@@ -9,7 +10,7 @@ default_path = os.getcwd()
 
 
 def download(url, directory=default_path):
-    """Download web page to the selected directory.
+    """Download web page and locals to the selected directory.
 
     Args:
         url: url of the web page.
@@ -24,3 +25,20 @@ def download(url, directory=default_path):
     with open(download_path, 'w') as new_file:
         new_file.write(response.text)
     return download_path
+
+
+def download_locals(downloads, url):
+    """Download local resources.
+
+    Args:
+        downloads: list of links and file paths for downloads.
+        url: url of the web page.
+    """
+    for each in downloads:
+        link, path = each
+        if not urlparse(link).netloc:
+            link = '{0}{1}'.format(urlparse(url).netloc, link)
+        response = requests.get(link, stream=True)
+        with open(path, 'wb') as local_file:
+            for chunk in response.iter_content(chunk_size=None):
+                local_file.write(chunk)
