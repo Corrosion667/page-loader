25b59c0dcb4fe117951c2fbcc38bf94fe86992e9
Artem Nechai <lenikol95@mail.ru>
2021-11-23 22:57:59 +0300

    create mock tests for main function

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,15 +16,42 @@ def test_html_name():
 
 
 def test_download(requests_mock, tmp_path):
-    """Test for download function: check file existing and its content.
+    """Test download function: check file existing, its content and local resources.
 
     Args:
         requests_mock: mock for HTTP request.
         tmp_path: temporary path for testing.
     """
-    requests_mock.get(TEST_URL, text='data')
-    download(TEST_URL, tmp_path)
-    file_name = html_name(TEST_URL)
-    with open(os.path.join(tmp_path, file_name)) as test_file:
+    with open('tests/fixtures/test_page.html') as web_page:
+        html_before = web_page.read()
+    with open('tests/fixtures/result_page.html') as result_page:
+        html_after = result_page.read()
+
+    requests_mock.get(TEST_URL, text=html_before)
+    requests_mock.get('https://ru.hexlet.io/assets/professions/nodejs.png')
+    requests_mock.get('https://ru.hexlet.io/assets/application.css')
+    requests_mock.get('https://ru.hexlet.io/packs/js/runtime.js')
+
+    download_path = download(TEST_URL, tmp_path)
+    image_path = os.path.join(
+        tmp_path, 'ru-hexlet-io-courses_files/ru-hexlet-io-assets-professions-nodejs.png',
+    )
+    css_path = os.path.join(
+        tmp_path, 'ru-hexlet-io-courses_files/ru-hexlet-io-assets-application.css',
+    )
+    js_path = os.path.join(
+        tmp_path, 'ru-hexlet-io-courses_files/ru-hexlet-io-packs-js-runtime.js',
+    )
+    html_path = os.path.join(
+        tmp_path, 'ru-hexlet-io-courses_files/ru-hexlet-io-courses.html',
+    )
+
+    assert os.path.exists(download_path)
+    assert os.path.exists(image_path)
+    assert os.path.exists(css_path)
+    assert os.path.exists(js_path)
+    assert os.path.exists(html_path)
+
+    with open(os.path.join(tmp_path, download_path)) as test_file:
         test_content = test_file.read()
-        assert test_content == 'data\n'
+        assert test_content == html_after
