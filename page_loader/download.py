5f4a45b004c7afbbff231468ba8b0a7329573c5e
Artem Nechai <lenikol95@mail.ru>
2021-11-27 19:20:59 +0300

    update download - bytes

--- page_loader/download.py
+++ page_loader/download.py
@@ -24,8 +24,8 @@ def download(url, directory=default_path):
     response = requests.get(url)
     file_name = html_name(url)
     download_path = os.path.join(directory, file_name)
-    with open(download_path, 'w') as new_file:
-        new_file.write(response.text)
+    with open(download_path, 'wb') as new_file:
+        new_file.write(response.content)
     downloads = get_and_replace_locals(download_path, url)
     download_locals(downloads, url, directory)
     return download_path
