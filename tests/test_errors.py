da0b0d7d36656624fe2dc6aff63a17822d4398b3
Artem Nechai <lenikol95@mail.ru>
2021-11-30 23:46:55 +0300

    create test for denied access

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -5,6 +5,7 @@ import os
 import pytest
 from page_loader.download import download
 
+UNACCESSABLE_RIGHTS = 000
 TEST_URL = 'https://ru.hexlet.io/courses'
 
 
@@ -17,3 +18,16 @@ def test_download_wrong_path(tmp_path):
     wrong_path = os.path.join(tmp_path, '/wrong_folder')
     with pytest.raises(FileNotFoundError):
         download(TEST_URL, wrong_path)
+
+
+def test_unaccessable(tmp_path):
+    """Do not allow to pass to programm unaccessable folders.
+
+    Args:
+        tmp_path: temporary path for testing.
+    """
+    unaccessable_path = os.path.join(tmp_path, 'wrong_folder')
+    os.mkdir(unaccessable_path)
+    os.chmod(unaccessable_path, UNACCESSABLE_RIGHTS)
+    with pytest.raises(PermissionError):
+        download(TEST_URL, unaccessable_path)
