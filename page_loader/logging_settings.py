5d8098eb1d8502e445d75357519001a86f062066
Artem Nechai <lenikol95@mail.ru>
2021-12-04 19:59:30 +0300

    create logging settings file

---
+++ page_loader/logging_settings.py
@@ -0,0 +1,26 @@
+"""Module with settings for logging."""
+logging_config = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    'formatters': {
+        'default_formatter': {
+            'format': '[%(levelname)s:%(asctime)s] %(message)s'
+        },
+    },
+
+    'handlers': {
+        'stream_handler': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default_formatter',
+        },
+    },
+
+    'loggers': {
+        'my_logger': {
+            'handlers': ['stream_handler'],
+            'level': 'DEBUG',
+            'propagate': True
+        }
+    }
+}
