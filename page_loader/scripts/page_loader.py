44d626ee3bdcd4c8d72bb52af47736d560ffce1a
Artem Nechai <lenikol95@mail.ru>
2021-12-04 21:19:28 +0300

    add logging to script

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 """This program downloads a page from the network."""
 import argparse
+import logging
+import logging.config
 
-from page_loader.download import default_path, download
+import requests
+from page_loader.download import ExpectedError, default_path, download
+from page_loader.logging_settings import LOGGING_CONFIG
 
 SUCCESS = "Page was successfully downloaded into '{0}'"
+
+logging.config.dictConfig(LOGGING_CONFIG)
+logger = logging.getLogger('default_logger')
 
 
 def main() -> None:
@@ -21,7 +28,12 @@ def main() -> None:
     )
     parser.add_argument('url', type=str)
     args = parser.parse_args()
-    path = download(args.url, args.output)
+    try:
+        path = download(args.url, args.output)
+    except (ExpectedError, requests.exceptions.RequestException) as err:
+        logger.error(err)
+    except Exception as err:
+        logger.error('Unknown {0} error happened'.format(str(err)))
     print(SUCCESS.format(path))
 
 
